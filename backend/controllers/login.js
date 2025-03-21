00304fc918017e0c0fe92ae576be716056785765
damodara2006 <damodara2006@gmail.com>
2025-03-21 19:05:39 +0530

    backend login completed

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -1,73 +1,70 @@
-import { LoginSchema } from "../models/Login.js";
-import AsyncHandler from "../utils/AsyncHandler.js";
-import ErrorApi from "../utils/ErrorApi.js";
-import bcrypt from "bcrypt";
-import cookieParser from "cookie-parser";
-import jwt from "jsonwebtoken"
-const login = AsyncHandler(async (req, res) => {
-  const { email, password } = req.body;
+    import { LoginSchema } from "../models/Login.js";
+    import AsyncHandler from "../utils/AsyncHandler.js";
+    import ErrorApi from "../utils/ErrorApi.js";
+    import bcrypt from "bcrypt";
+    import cookieParser from "cookie-parser";
+    import jwt from "jsonwebtoken";
+    const login = AsyncHandler(async (req, res) => {
+      const { email, password } = req.body;
 
-  const user = await LoginSchema.findOne({ email: email });
+      const user = await LoginSchema.findOne({ email: email });
 
-  if (!user) {
-    return res.send("No user found");
-  }
-  const token = await jwt.sign( {user} , "json-web-token" )
-  res.cookie( "Logeduser" ,token , {maxAge: 100000 , httpOnly:true})
+      if (!user) {
+        return res.send("No user found");
+      }
+      const token = await jwt.sign({ user }, "json-web-token");
+      res.cookie("Logeduser", token, { maxAge: 100000, httpOnly: true });
 
-  const userpass = user.password;
+      const userpass = user.password;
 
-
-
-  if (await bcrypt.compare(password, userpass)) {
-    return res.send("Loggedin");
-  } else {
-    return res.send("Password incorrect");
-  }
-  return res.send(user.password);
-});
-
-const newuser = AsyncHandler(async (req, res) => {
-  let { email, password, username } = req.body;
-
-  if ([email, password, username].some((item) => item == "")) {
-    return res.send("All fields required");
-  }
-
-  const useremail = await LoginSchema.findOne({
-    $or: [{ email }]
-  });
-  const userusername = await LoginSchema.findOne({
-    $or: [{ username }]
-  });
-
-  if (useremail) {
-    return res.send("Email Already exist");
-  }
-  if (userusername) {
-    return res.send("Username Already exist");
-  }
-
-  if (!userusername) {
-    password = await bcrypt.hash(password, 10);
-    const newuser = new LoginSchema({
-      email: email,
-      password: password,
-      username: username
+      if (await bcrypt.compare(password, userpass)) {
+        return res.send("Loggedin");
+      } else {
+        return res.send("Password incorrect");
+      }
+      return res.send(user.password);
     });
-    await newuser.save();
-    res.send(newuser);
-  }
 
-  return res.send(userusername);
-});
+    const newuser = AsyncHandler(async (req, res) => {
+      let { email, password, username } = req.body;
 
-const logout = AsyncHandler(async(req,res)=>{
+      if ([email, password, username].some((item) => item == "")) {
+        return res.send("All fields required");
+      }
 
-  const cooks = req.cookies;
- await res.clearCookie("Logeduser"); 
+      const useremail = await LoginSchema.findOne({
+        $or: [{ email }]
+      });
+      const userusername = await LoginSchema.findOne({
+        $or: [{ username }]
+      });
 
-  const cook = req.cookies;
-  console.log(cook)
-})
-export { newuser, login ,logout};
+      if (useremail) {
+        return res.send("Email Already exist");
+      }
+      if (userusername) {
+        return res.send("Username Already exist");
+      }
+
+      if (!userusername) {
+        password = await bcrypt.hash(password, 10);
+        const newuser = new LoginSchema({
+          email: email,
+          password: password,
+          username: username
+        });
+        await newuser.save();
+        res.send(newuser);
+      }
+
+      return res.send(userusername);
+    });
+
+    const logout = AsyncHandler(async (req, res) => {
+      const cooks = req.cookies;
+      await res.clearCookie("Logeduser");
+
+      const cook = req.cookies;
+      console.log(cook);
+    });
+    export { newuser, login, logout };
