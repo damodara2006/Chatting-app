2d7398462adda1988c7ec377b5efbf225dab7e5e
damodara2006 <damodara2006@gmail.com>
2025-03-22 23:54:22 +0530

    Login user and can change profile picture

--- backend/controllers/changedata.js
+++ backend/controllers/changedata.js
@@ -1,12 +1,15 @@
 import AsyncHandler from "../utils/AsyncHandler.js";
 import { LoginSchema } from "../models/Login.js";
-
+import jwt from  "jsonwebtoken"
 const changedata = AsyncHandler(async(req,res)=>{
     let file = req.filelink
     let email = req.email
     await LoginSchema.findOneAndUpdate({email:email }).updateOne({profile:file})
     const user = await LoginSchema.findOne({email:email})
-    res.send(user)
+    const token = await jwt.sign( {user} , "json-web-token" );
+          console.log(token)
+          res.cookie("Logeduser", token, { maxAge: 1200000 , secure:false});
+   return res.send(user)
 })
 
 export default changedata
